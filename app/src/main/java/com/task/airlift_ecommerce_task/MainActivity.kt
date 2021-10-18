0e74be3aa873608d3aa237909445a352fdacd9d8
Umer Khan <umerkhan4551@gmail.com>
2021-10-18 23:31:51 +0500

    Initial Commit

---
+++ app/src/main/java/com/task/airlift_ecommerce_task/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.task.airlift_ecommerce_task
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
